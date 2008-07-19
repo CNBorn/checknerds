1ff7265d91709703e4705b3bf5658144de99f915
CNBorn <CNBorn@hotmail.com>
2008-07-19 16:31:20 +0800

    Starting using templating system.

--- hello.py
+++ hello.py
@@ -3,6 +3,11 @@ import wsgiref.handlers
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+import os
+import datetime
+from google.appengine.ext.webapp import template
+
+
 
 class tarsusaItem(db.Model):
     user = db.UserProperty()
@@ -22,29 +27,65 @@ class MainPage(webapp.RequestHandler):
 	def get(self):
 		user = users.get_current_user()	
 		
-		if user:
-		
-			self.response.out.write ('<html><body>now begin to process the first tarsusa item!<BR><BR>')
+		if user != None:
 
-			tarsusaItemCollection = db.GqlQuery("SELECT * FROM tarsusaItem ORDER BY date DESC LIMIT 10")
+			# Show His Daily Routine.
+			tarsusaItemCollection = db.GqlQuery("SELECT * FROM tarsusaItem WHERE user = :1 and routine = 'daily' ORDER BY date DESC",
+                               users.get_current_user())
 
-			for tarsusaItem in tarsusaItemCollection:
-				if tarsusaItem.user:
-					self.response.out.write('<b>%s</b> wrote:' % tarsusaItem.user.nickname())
-				else:
-					self.response.out.write('An anonymous person wrote:')
+			#for tarsusaItem in tarsusaItemCollection:
+			#		self.response.out.write('<b>%s</b> wrote:' % tarsusaItem.user.nickname())
+			#	self.response.out.write('<blockquote>%s</blockquote>' %
+            #       cgi.escape(tarsusaItem.comment))
+			
+			template_values = {
+				'UserNickName': user.nickname(),
+				'tarsusaItemCollection': tarsusaItemCollection,
+				'htmltag_today': datetime.datetime.date(datetime.datetime.now()), 
+				}
 
-				self.response.out.write('<blockquote>%s</blockquote>' %
-                    cgi.escape(tarsusaItem.name))
+			# For LoggedIn User, Show his own items.
+			#self.response.out.write ('<html><body>now begin to process the first tarsusa item!<BR><BR>')
 
-				self.response.out.write('<blockquote>%s</blockquote>' %
-                    cgi.escape(tarsusaItem.comment))
+			#tarsusaItemCollection = db.GqlQuery("SELECT * FROM tarsusaItem ORDER BY date DESC LIMIT 10")
+
+			#for tarsusaItem in tarsusaItemCollection:
+			#	if tarsusaItem.user: 
+			#		self.response.out.write('<b>%s</b> wrote:' % tarsusaItem.user.nickname())
+			#	else:
+			#		self.response.out.write('An anonymous person wrote:')
+
+			#	self.response.out.write('<blockquote>%s</blockquote>' %
+            #        cgi.escape(tarsusaItem.name))
+
+			#	self.response.out.write('<blockquote>%s</blockquote>' %
+            #       cgi.escape(tarsusaItem.comment))
+			
+			path = os.path.join(os.path.dirname(__file__), 'index.html')
+			self.response.out.write(template.render("./index.html", template_values))
 	
 
-			self.response.out.write ('<a href="/Add">Add an Item Now!</a>')
+			#self.response.out.write ('<a href="/Add">Add an Item Now!</a>')
 
-	    #else:
-    	#	self.redirect(users.create_login_url(self.request.uri))
+		else:
+    	
+			tarsusaItemCollection = db.GqlQuery("SELECT * FROM tarsusaItem WHERE public=True ORDER BY date DESC LIMIT 10")
+
+			#for tarsusaItem in tarsusaItemCollection:
+				#self.response.out.write('An anonymous person wrote:')
+
+				#self.response.out.write('<blockquote>%s</blockquote>' %
+                #    cgi.escape(tarsusaItem.name))
+
+				#self.response.out.write('<blockquote>%s</blockquote>' %
+                #    cgi.escape(tarsusaItem.comment))
+
+		
+		
+		#Manupilating Templates	
+		#path = os.path.join(os.path.dirname(__file__), 'index.html')
+
+		#self.redirect(users.create_login_url(self.request.uri))
 
 class AddPage(webapp.RequestHandler):
 	def get(self):
@@ -58,9 +99,18 @@ class AddPage(webapp.RequestHandler):
 		标题  <input type="text" name="name" value="" size="18" class="sl"><br />
 		内容  <textarea name="comment" rows="4" cols="16" wrap="PHYSICAL" class="ml"></textarea><br />
 		类别  <input type="text" name="tag" size="18" class="sl"><br />
-		预计完成于<br /></body></html>''')
-	
-					# first_tarsusa_item.name
+		预计完成于<br />''')
+			self.response.out.write ('''性质：<select name="routine">
+									<option value="none" selected="selected">非坚持性任务</option>
+									<option value="daily">每天</option>
+									<option value="weekly">每周</option>
+									<option value="monthly">每月</option>
+									<option value="seasonly">每季度</option>
+									<option value="yearly">每年</option>
+									</select><br>''')
+			self.response.out.write ('<input type="checkbox" name="public" value="True">公开项目<BR>')
+
+			# first_tarsusa_item.name
 			#print first_tarsusa_item.comment
 			#print first_tarsusa_item.routine
 			self.response.out.write ('''<input type="submit" name="submit" value="添加一个任务"></form>''')
@@ -70,8 +120,12 @@ class AddPage(webapp.RequestHandler):
 class AddItemProcess(webapp.RequestHandler):
 	def post(self):
 		first_tarsusa_item = tarsusaItem(user=users.get_current_user(),name=cgi.escape(self.request.get('name')), comment=cgi.escape(self.request.get('comment')), tag=cgi.escape(self.request.get('tag')),
-										routine="none", 
-										public=False)
+										routine=cgi.escape(self.request.get('routine')))
+		if self.request.get('public') == "True":
+			first_tarsusa_item.public = True
+		else:
+			first_tarsusa_item.public = False
+										
 		first_tarsusa_item.put()
 		self.redirect('/')
 
