1bcb3ae202368d0b2c0cdc3e7b30f00796973d3f
CNBorn <CNBorn@hotmail.com>
2008-08-26 22:38:35 +0800

    Trying to upload this project on: checknerds.appspot.com, after a lot of tries, finally success after removing all handler information for pages directory.

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+
+#from django.conf import settings
+#settings._target = None
+import os
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 import cgi
 import wsgiref.handlers
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
-import os
+
 import datetime
 import string
 from google.appengine.ext.webapp import template
