9f47cefab1eea8d909c3017f0a629346ccf11b72
cnborn <cnborn@oz>
2009-11-29 14:09:00 +0800

    add the cycle image in welcome page

--- miscellaneous.py
+++ miscellaneous.py
@@ -31,271 +31,272 @@ from google.appengine.api import memcache
 import logging
 
 class GuestbookPage(tarsusaRequestHandler):
-	def get(self):
-		strAboutPageTitle = "CheckNerds项目 - Guestbook"
-		strAboutPageContent = '''<br /><iframe src="http://spreadsheets.google.com/embeddedform?key=pWd4_W1-LSL4xnGNRuHq6JA" width="95%" height="735" frameborder="0" marginheight="0" marginwidth="0">正在加载...</iframe><BR>
-		
-		'''
-		## Get Current User.
-		# code below are comming from GAE example
-		q = db.GqlQuery("SELECT * FROM tarsusaUser WHERE user = :1", users.get_current_user())
-		CurrentUser = q.get()
-	
+    def get(self):
+        strAboutPageTitle = "CheckNerds项目 - Guestbook"
+        strAboutPageContent = '''<br /><iframe src="http://spreadsheets.google.com/embeddedform?key=pWd4_W1-LSL4xnGNRuHq6JA" width="95%" height="735" frameborder="0" marginheight="0" marginwidth="0">正在加载...</iframe><BR>
+        
+        '''
+        ## Get Current User.
+        # code below are comming from GAE example
+        q = db.GqlQuery("SELECT * FROM tarsusaUser WHERE user = :1", users.get_current_user())
+        CurrentUser = q.get()
+    
 
-		try:
+        try:
 
-			template_values = {
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(users.get_current_user().nickname()),
-					'UserID': CurrentUser.key().id(),
-					'singlePageTitle': strAboutPageTitle,
-					'singlePageContent': strAboutPageContent,
-			}
-		
-		except:
+            template_values = {
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(users.get_current_user().nickname()),
+                    'UserID': CurrentUser.key().id(),
+                    'singlePageTitle': strAboutPageTitle,
+                    'singlePageContent': strAboutPageContent,
+            }
+        
+        except:
 
-			
-			template_values = {
-				
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-				'singlePageTitle': strAboutPageTitle,
-				'singlePageContent': strAboutPageContent,
+            
+            template_values = {
+                
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+                'singlePageTitle': strAboutPageTitle,
+                'singlePageContent': strAboutPageContent,
 
-			}
+            }
 
 
-	
-		path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
-		self.response.out.write(template.render(path, template_values))
+    
+        path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
+        self.response.out.write(template.render(path, template_values))
 
 class BlogPage(tarsusaRequestHandler):
-	def get(self):
-		
-		#from google.appengine.api import urlfetch
-		# GAE中对远程网页的获取不能通过urllib，只能通过google自己的urlfetch   
+    def get(self):
+        
+        #from google.appengine.api import urlfetch
+        # GAE中对远程网页的获取不能通过urllib，只能通过google自己的urlfetch   
 
-		#url = "http://feed.feedsky.com/cnborn"
-		#result = urlfetch.fetch(url)
-		
-		#if result.status_code == 200:
-			
-			#import xml.sax
-			#from xml.dom import minidom
-			#xmldoc = minidom.parseString(result.content.decode('utf-8')))
+        #url = "http://feed.feedsky.com/cnborn"
+        #result = urlfetch.fetch(url)
+        
+        #if result.status_code == 200:
+            
+            #import xml.sax
+            #from xml.dom import minidom
+            #xmldoc = minidom.parseString(result.content.decode('utf-8')))
 
-			#parser = rss_parser()
-			#strAboutPageContent = result.content.decode('utf-8')
+            #parser = rss_parser()
+            #strAboutPageContent = result.content.decode('utf-8')
 
 
-		#strAboutPageContent = '''Coming soon.<BR><BR>''' #+ d.entries[0].title + d.entries[0].link + d.entries[0].description + d.entries[0].date + d.entries[0].date_parsed + d.entries[0].id
+        #strAboutPageContent = '''Coming soon.<BR><BR>''' #+ d.entries[0].title + d.entries[0].link + d.entries[0].description + d.entries[0].date + d.entries[0].date_parsed + d.entries[0].id
 
-		strAboutPageContent = '''<div id="twitter_div"><ul id="twitter_update_list"></ul></div>
-								<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
-								<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/checknerds.json?callback=twitterCallback2&amp;count=10"></script>'''
+        strAboutPageContent = '''<div id="twitter_div"><ul id="twitter_update_list"></ul></div>
+                                <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+                                <script type="text/javascript" src="http://twitter.com/statuses/user_timeline/checknerds.json?callback=twitterCallback2&amp;count=10"></script>'''
 
-		strAboutPageTitle = "CheckNerds - Recent Updates (powered by Twitter)"
-		
-		if self.chk_login():
-			CurrentUser = self.get_user_db()
-			template_values = {
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(users.get_current_user().nickname()),
-					'UserID': CurrentUser.key().id(),
-					'singlePageTitle': strAboutPageTitle,
-					'singlePageContent': strAboutPageContent,
-			}
-		else:
-			template_values = {				
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-				'singlePageTitle': strAboutPageTitle,
-				'singlePageContent': strAboutPageContent,
-			}
+        strAboutPageTitle = "CheckNerds - Recent Updates (powered by Twitter)"
+        
+        if self.chk_login():
+            CurrentUser = self.get_user_db()
+            template_values = {
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(users.get_current_user().nickname()),
+                    'UserID': CurrentUser.key().id(),
+                    'singlePageTitle': strAboutPageTitle,
+                    'singlePageContent': strAboutPageContent,
+            }
+        else:
+            template_values = {             
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+                'singlePageTitle': strAboutPageTitle,
+                'singlePageContent': strAboutPageContent,
+            }
 
-	
-		path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
-		self.response.out.write(template.render(path, template_values))
+    
+        path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
+        self.response.out.write(template.render(path, template_values))
 
 class AboutPage(tarsusaRequestHandler):
-	def get(self):
-		
-		import os
-		strdevVersion = os.environ['CURRENT_VERSION_ID']
+    def get(self):
+        
+        import os
+        strdevVersion = os.environ['CURRENT_VERSION_ID']
 
-		# New CheckLogin code built in tarsusaRequestHandler 
-		if self.chk_login():
-			CurrentUser = self.get_user_db()		
-			template_values = {
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(CurrentUser.dispname),
-					'UserID': CurrentUser.key().id(),
-					'devVersion': strdevVersion,
-			}
-		
-		else:			
-			template_values = {
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-				'devVersion': strdevVersion,
-			}
-	
-		path = os.path.join(os.path.dirname(__file__), 'pages/about.html')
-		self.response.out.write(template.render(path, template_values))
+        # New CheckLogin code built in tarsusaRequestHandler 
+        if self.chk_login():
+            CurrentUser = self.get_user_db()        
+            template_values = {
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(CurrentUser.dispname),
+                    'UserID': CurrentUser.key().id(),
+                    'devVersion': strdevVersion,
+            }
+        
+        else:           
+            template_values = {
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+                'devVersion': strdevVersion,
+            }
+    
+        path = os.path.join(os.path.dirname(__file__), 'pages/about.html')
+        self.response.out.write(template.render(path, template_values))
 
 class StatsticsPage(tarsusaRequestHandler):
-	def get(self):
-		
-		# Show statstics information.
+    def get(self):
+        
+        # Show statstics information.
 
-		if self.chk_login():
-			CurrentUser = self.get_user_db()
-		else:
-			self.redirect('/')
-		
-		TotalUserCount = db.GqlQuery("SELECT * FROM tarsusaUser").count()
-		TotaltarsusaItem = db.GqlQuery("SELECT * FROM tarsusaItem").count()
-		
-		htmltag = ''
-		htmltag += 'Uptime: ' + str(datetime.datetime.now() - datetime.datetime(2008,8,26,20,0,0))
-		htmltag += '<br />Project Started Since: ' + str(datetime.date.today() - datetime.date(2008, 7, 19)) + ' ago.'
-		htmltag += '<br />User Account: ' + str(TotalUserCount)
-		htmltag += '<br />Total Items: ' + str(TotaltarsusaItem)
+        if self.chk_login():
+            CurrentUser = self.get_user_db()
+        else:
+            self.redirect('/')
+        
+        TotalUserCount = db.GqlQuery("SELECT * FROM tarsusaUser").count()
+        TotaltarsusaItem = db.GqlQuery("SELECT * FROM tarsusaItem").count()
+        
+        htmltag = ''
+        htmltag += 'Uptime: ' + str(datetime.datetime.now() - datetime.datetime(2008,8,26,20,0,0))
+        htmltag += '<br />Project Started Since: ' + str(datetime.date.today() - datetime.date(2008, 7, 19)) + ' ago.'
+        htmltag += '<br />User Account: ' + str(TotalUserCount)
+        htmltag += '<br />Total Items: ' + str(TotaltarsusaItem)
 
-		try:
-			htmltag += '<br /><br /><b>memcached stats:</b>'
-			stats = memcache.get_stats()    
-			htmltag += "<br /><b>Cache Hits:</b>" + str(stats['hits'])
-			htmltag += "<br /><b>Cache Misses:</b>" +str(stats['misses'])					
-			htmltag += "<br /><b>Total Requested Cache bytes:</b>" +str(stats['byte_hits'])
-			htmltag += "<br /><b>Total Cache items:</b>" +str(stats['items'])
-			htmltag += "<br /><b>Total Cache bytes:</b>" +str(stats['bytes'])
-			htmltag += "<br /><b>Oldest Cache items:</b>" +str(stats['oldest_item_age'])
-		except:
-			pass
+        try:
+            htmltag += '<br /><br /><b>memcached stats:</b>'
+            stats = memcache.get_stats()    
+            htmltag += "<br /><b>Cache Hits:</b>" + str(stats['hits'])
+            htmltag += "<br /><b>Cache Misses:</b>" +str(stats['misses'])                   
+            htmltag += "<br /><b>Total Requested Cache bytes:</b>" +str(stats['byte_hits'])
+            htmltag += "<br /><b>Total Cache items:</b>" +str(stats['items'])
+            htmltag += "<br /><b>Total Cache bytes:</b>" +str(stats['bytes'])
+            htmltag += "<br /><b>Oldest Cache items:</b>" +str(stats['oldest_item_age'])
+        except:
+            pass
 
-		template_values = {
-			'UserLoggedIn': 'Logged In',				
-			'UserNickName': cgi.escape(self.login_user.nickname()),
-			'UserID': CurrentUser.key().id(),	
-			
-			'singlePageTitle': 'Statstics',
-			'singlePageContent': htmltag,
+        template_values = {
+            'UserLoggedIn': 'Logged In',                
+            'UserNickName': cgi.escape(self.login_user.nickname()),
+            'UserID': CurrentUser.key().id(),   
+            
+            'singlePageTitle': 'Statstics',
+            'singlePageContent': htmltag,
 
-			'htmltag_today': datetime.datetime.date(datetime.datetime.now()), 
-			'htmltag_TotalUser': TotalUserCount,
-			'htmltag_TotaltarsusaItem': TotaltarsusaItem,
+            'htmltag_today': datetime.datetime.date(datetime.datetime.now()), 
+            'htmltag_TotalUser': TotalUserCount,
+            'htmltag_TotaltarsusaItem': TotaltarsusaItem,
 
-		}
-		
-		#Manupilating Templates	
-		path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
-		self.response.out.write(template.render(path, template_values))
+        }
+        
+        #Manupilating Templates 
+        path = os.path.join(os.path.dirname(__file__), 'pages/simple_page.html')
+        self.response.out.write(template.render(path, template_values))
 
 class DocsPage(tarsusaRequestHandler):
-	def get(self):
+    def get(self):
 
-		# New CheckLogin code built in tarsusaRequestHandler 
-		if self.chk_login():
-			CurrentUser = self.get_user_db()		
-			template_values = {
-					'PrefixCSSdir': "/",
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(CurrentUser.dispname),
-					'UserID': CurrentUser.key().id(),
-			}
-		
-		else:			
-			template_values = {
-				'PrefixCSSdir': "/",
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-			}
-		
-		pageid = self.request.path[len('/docs/'):]
-		if pageid == '':
-			pageid = 'index'
-		path = os.path.join(os.path.dirname(__file__), 'pages/docs/' + pageid + '.html')
+        # New CheckLogin code built in tarsusaRequestHandler 
+        if self.chk_login():
+            CurrentUser = self.get_user_db()        
+            template_values = {
+                    'PrefixCSSdir': "/",
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(CurrentUser.dispname),
+                    'UserID': CurrentUser.key().id(),
+            }
+        
+        else:           
+            template_values = {
+                'PrefixCSSdir': "/",
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+            }
+        
+        pageid = self.request.path[len('/docs/'):]
+        if pageid == '':
+            pageid = 'index'
+        path = os.path.join(os.path.dirname(__file__), 'pages/docs/' + pageid + '.html')
 
-		self.response.out.write(template.render(path, template_values))
+        self.response.out.write(template.render(path, template_values))
 
 class LabsPage(tarsusaRequestHandler):
-	def get(self):
+    def get(self):
 
-		# New CheckLogin code built in tarsusaRequestHandler 
-		if self.chk_login():
-			CurrentUser = self.get_user_db()		
-			template_values = {
-					'PrefixCSSdir': "/",
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(CurrentUser.dispname),
-					'UserID': CurrentUser.key().id(),
-			}
-		
-		else:			
-			template_values = {
-				'PrefixCSSdir': "/",
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-			}
-		
-		pageid = self.request.path[len('/docs/'):]
-		if pageid == '':
-			pageid = 'index'
-		path = os.path.join(os.path.dirname(__file__), 'pages/labs/index.html')
+        # New CheckLogin code built in tarsusaRequestHandler 
+        if self.chk_login():
+            CurrentUser = self.get_user_db()        
+            template_values = {
+                    'PrefixCSSdir': "/",
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(CurrentUser.dispname),
+                    'UserID': CurrentUser.key().id(),
+            }
+        
+        else:           
+            template_values = {
+                'PrefixCSSdir': "/",
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+            }
+        
+        pageid = self.request.path[len('/docs/'):]
+        if pageid == '':
+            pageid = 'index'
+        path = os.path.join(os.path.dirname(__file__), 'pages/labs/index.html')
 
-		self.response.out.write(template.render(path, template_values))
-	
+        self.response.out.write(template.render(path, template_values))
+    
 
 
 class FlushCache(tarsusaRequestHandler):
-	def get(self):
-		memcache.flush_all()
-		logging.info("Memcache Flushed.")
-		self.redirect('/')
+    def get(self):
+        memcache.flush_all()
+        logging.info("Memcache Flushed.")
+        self.redirect('/')
 
 class CaliforniaPage(tarsusaRequestHandler):
-	def get(self):
+    def get(self):
 
-		# New CheckLogin code built in tarsusaRequestHandler 
-		if self.chk_login():
-			CurrentUser = self.get_user_db()
-			import tarsusaCore
-			tarsusaItemCollection_UserTodoItems = tarsusaCore.get_tarsusaItemCollection(CurrentUser.key().id(), False, maxitems=25)
+        # New CheckLogin code built in tarsusaRequestHandler 
+        if self.chk_login():
+            CurrentUser = self.get_user_db()
+            import tarsusaCore
+            tarsusaItemCollection_UserTodoItems = tarsusaCore.get_tarsusaItemCollection(CurrentUser.key().id(), False, maxitems=25)
 
-			tarsusaUserFriends = tarsusaCore.get_UserFriends(CurrentUser.key().id())
-			
-			template_values = {
-					'PrefixCSSdir': "/",
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(CurrentUser.dispname),
-					'UserID': CurrentUser.key().id(),
-					'tarsusaItemCollection_UserToDoItems': tarsusaItemCollection_UserTodoItems,
-					'tarsusaUserFriends': tarsusaUserFriends,
-					'htmltag_today': datetime.date.today(),
-			}
-		
-		else:			
-			template_values = {
-				'PrefixCSSdir': "/",
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-			}
-	
-		path = os.path.join(os.path.dirname(__file__), 'pages/calit2/index.html')
-		self.response.out.write(template.render(path, template_values))	
+            tarsusaUserFriends = tarsusaCore.get_UserFriends(CurrentUser.key().id())
+            
+            template_values = {
+                    'PrefixCSSdir': "/",
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(CurrentUser.dispname),
+                    'UserID': CurrentUser.key().id(),
+                    'tarsusaItemCollection_UserToDoItems': tarsusaItemCollection_UserTodoItems,
+                    'tarsusaUserFriends': tarsusaUserFriends,
+                    'htmltag_today': datetime.date.today(),
+            }
+            path = os.path.join(os.path.dirname(__file__), 'pages/calit2/index.html')
+        
+        else:           
+            template_values = {
+                'PrefixCSSdir': "/",
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+            }
+            path = os.path.join(os.path.dirname(__file__), 'pages/calit2/welcome.html')
+    
+        self.response.out.write(template.render(path, template_values)) 
 
 def main():
-	application = webapp.WSGIApplication([('/about',AboutPage),
-								       ('/blog',BlogPage),
-									   ('/docs.+', DocsPage),
-									   ('/labs.+', LabsPage),
-								       ('/statstics',StatsticsPage),
-									   ('/flushcache', FlushCache),
-									   ('/california', CaliforniaPage),
-									   ('/guestbook', GuestbookPage)],
+    application = webapp.WSGIApplication([('/about',AboutPage),
+                                       ('/blog',BlogPage),
+                                       ('/docs.+', DocsPage),
+                                       ('/labs.+', LabsPage),
+                                       ('/statstics',StatsticsPage),
+                                       ('/flushcache', FlushCache),
+                                       ('/california', CaliforniaPage),
+                                       ('/guestbook', GuestbookPage)],
                                        debug=True)
-	wsgiref.handlers.CGIHandler().run(application)
+    wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
       main()
