53b815f65dd95f5c62e9c2662f2b96a1e5798f9b
CNBorn <CNBorn@hotmail.com>
2011-05-22 01:50:24 +0800

    Add Cache Decorator

--- tarsusaCore.py
+++ tarsusaCore.py
@@ -25,6 +25,7 @@ from datetime import timedelta
 import random
 
 import memcache
+import re
 
 import shardingcounter
 
@@ -150,13 +151,41 @@ def get_tarsusaItemCollection(userid, done, routine='none', startdate='', enddat
 
     return Item_List
 
+def gen_key_fac(in_key):
+    def gen_key(*args, **kwargs):
+        key = in_key
+        para_re = re.compile(r"{(.*?)}")
+        para_name_list = para_re.findall(key)
+        for pos, para in enumerate(para_name_list):
+            if para in kwargs.keys():
+                value = kwargs[para]
+                key = key.replace("{%s}" % para, str(value))
+            elif pos <= len(args):
+                value = args[pos]
+                key = key.replace("{%s}" % para, str(value))
+            else:
+                raise
+        return key
+    return gen_key
 
+
+def cache(key="default_mc_key", time=60*60*30):
+    def _cache(func):
+        gen_key=gen_key_fac(key)
+        def _processor(*args, **kwargs):
+            mkey = gen_key(*args, **kwargs)
+            is_cached = memcache.get(mkey)
+            if is_cached:
+                return memcache.get(mkey)
+            result = func(*args, **kwargs)
+            memcache.set(mkey, result, time)
+            return result
+        return _processor
+    return _cache
+
+@cache("itemlist:{user_id}", 3600)
 def get_undone_items(user_id, maxitems=100):
-    cached_userundoneitems = memcache.get_item("itemlist", user_id)
-    if cached_userundoneitems:
-        return cached_userundoneitems
     undone_items = get_tarsusaItemCollection(user_id, done=False, maxitems=maxitems)
-    memcache.set_item("itemlist", undone_items, user_id)
     return undone_items
 
 def _get_more_undone_items(user_id, maxitems, after_item_id):
