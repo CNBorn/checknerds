7b5dd6382a0b384fe1aab7a4a854cc5901ecb9dd
CNBorn <CNBorn@hotmail.com>
2011-05-28 00:50:34 +0800

    cleaned up tarsusaCore

--- tarsusaCore.py
+++ tarsusaCore.py
@@ -7,9 +7,7 @@
 #
 # ****************************************************************
 
-import urllib
 import cgi
-import wsgiref.handlers
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
