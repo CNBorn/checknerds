5e4447c84750234d8fe78517fc0438d0351ecd35
Tyler Xing <CNBorn@hotmail.com>
2011-06-22 23:01:37 +0800

    fixed 404 bug.

--- views/errorpages.py
+++ views/errorpages.py
@@ -10,30 +10,32 @@ from base import *
 import os
 
 class Error404(tarsusaRequestHandler):
-	def get(self):
-		self.error(404)
-		if self.chk_login():
-			CurrentUser = self.get_user_db()		
-			template_values = {
-					'UserLoggedIn': 'Logged In',
-					'UserNickName': cgi.escape(CurrentUser.dispname),
-					'UserID': CurrentUser.key().id(),
-			}
-		
-		else:			
-			template_values = {
-				'UserNickName': "访客",
-				'AnonymousVisitor': "Yes",
-			}
+    def get(self):
+        self.error(404)
+        if self.chk_login():
+            CurrentUser = self.get_user_db()        
+            template_values = {
+                    'UserLoggedIn': 'Logged In',
+                    'UserNickName': cgi.escape(CurrentUser.dispname),
+                    'UserID': CurrentUser.key().id(),
+            }
+        
+        else:           
+            template_values = {
+                'UserNickName': "访客",
+                'AnonymousVisitor': "Yes",
+            }
 
-		path = 'pages/error_404.html'
-		self.response.out.write(template.render(path, template_values))	
+        template_values['singlePageTitle'] = "页面没有找到"
+        template_values['singlePageContent'] = "此页面内容不存在"
+        path = '../pages/calit2/simple_page.html'
+        self.response.out.write(template.render(path, template_values)) 
 
 def main():
-	application = webapp.WSGIApplication([('/.*', Error404)]
-									   , debug=True)
+    application = webapp.WSGIApplication([('/.*', Error404)]
+                                       , debug=True)
 
-	wsgiref.handlers.CGIHandler().run(application)
+    wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
       main()
