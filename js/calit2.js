ce713ff38e22df7116799651dbd7c8a7d9d8ac71
CNBorn <CNBorn@hotmail.com>
2011-12-04 16:38:18 +0800

    added qunit for js test

---
+++ js/calit2.js
@@ -0,0 +1,8 @@
+var is_today_panel = function() {
+    return $("#tab_dailyroutine").hasClass("activenavitab");
+}
+var is_tomorrow_panel = function() {
+    return $("#tab_tomorrow").hasClass("activenavitab");
+}
+
+
