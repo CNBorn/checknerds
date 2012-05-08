fa88af44c87254ece521e5ea6f7114ecf9340974
CNBorn <CNBorn@hotmail.com>
2012-05-08 16:12:19 +0800

    clean code

--- views/miscellaneous.py
+++ views/miscellaneous.py
@@ -1,57 +1,13 @@
 # -*- coding: utf-8 -*-
-
-# ************************************************************* 
-# CheckNerds - www.checknerds.com
-# version 1.0, codename California
-# - miscellaneous.py
-# Copyright (C) CNBorn, 2008-2010
-# http://cnborn.net, http://twitter.com/CNBorn
-# ************************************************************* 
-
 import os
-import sys
-
 import cgi
-import wsgiref.handlers
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext import db
-from google.appengine.ext.webapp import template
-
-import datetime
-
-from models import *
-from base import *
-
-import memcache
 import logging
 
-class GuestbookPage(tarsusaRequestHandler):
-    def get(self):
-        strAboutPageTitle = "Guestbook"
-        strAboutPageContent = '''<br /><iframe src="http://spreadsheets.google.com/embeddedform?key=pWd4_W1-LSL4xnGNRuHq6JA" width="95%" height="735" frameborder="0" marginheight="0" marginwidth="0">正在加载...</iframe><BR>
-        
-        '''
-        if self.chk_login():
-            CurrentUser = self.get_user_db()        
-            template_values = {
-                    'UserLoggedIn': 'Logged In',
-                    'UserNickName': cgi.escape(CurrentUser.dispname),
-                    'UserID': CurrentUser.key().id(),
-                    'singlePageTitle': strAboutPageTitle,
-                    'singlePageContent': strAboutPageContent,
-            }
-        
-        else:
-            template_values = {
-                'UserNickName': "访客",
-                'AnonymousVisitor': "Yes",
-                'singlePageTitle': strAboutPageTitle,
-                'singlePageContent': strAboutPageContent,
-            }
+import wsgiref.handlers
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 
-        path = os.path.join(os.path.dirname(__file__), '../pages/calit2/simple_page.html')
-        self.write(template.render(path, template_values))
+from base import tarsusaRequestHandler
 
 class AboutPage(tarsusaRequestHandler):
     def get(self):
@@ -148,8 +104,7 @@ def main():
                                        ('/docs.+', DocsPage),
                                        ('/labs.+', LabsPage),
                                        ('/flushcache', FlushCache),
-                                       ('/cron_flushcache', cron_flushcache),
-                                       ('/guestbook', GuestbookPage)],
+                                       ('/cron_flushcache', cron_flushcache)],
                                        debug=True)
     wsgiref.handlers.CGIHandler().run(application)
 
