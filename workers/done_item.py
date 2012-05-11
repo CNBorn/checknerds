d6f1128934edb878535dc431759727993f154a37
CNBorn <CNBorn@hotmail.com>
2012-05-11 22:56:01 +0800

    fix bug for done_item worker

--- workers/done_item.py
+++ workers/done_item.py
@@ -9,16 +9,18 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class DoneItemWorker(webapp.RequestHandler):
     def post(self):
+        user_id = self.request.get('user_id')
+        item_id = self.request.get('item_id')
+        misc = self.request.get('misc')
+
         def _done_item():
             from models.user import get_user
             from models.item import tarsusaItem
-            user_id = self.request.get('user_id')
-            item_id = self.request.get('item_id')
-            misc = self.request.get('misc')
 
             user = get_user(user_id)
             item = tarsusaItem.get_item(item_id) 
             item.done_item(user, misc)
+
         db.run_in_transaction(_done_item)
 
 
